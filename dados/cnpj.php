e895c0416a945bdabacb7f74c35db0c8c625d834
Rodrigo Portillo <rodrigo@portillodesign.com.br>
2020-09-23 20:42:51 -0300

    Update cnpj.php

--- dados/cnpj.php
+++ dados/cnpj.php
@@ -5,10 +5,12 @@ header("Content-Type: text/plain");
 
 //Capturar CNPJ
 $cnpj = $_REQUEST["cnpj"];
+$cnpj = preg_replace('/\D/', '', $cnpj); //Retira os caracteres que não são dígitos
 
 ///Criando Comunicação cURL
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://www.receitaws.com.br/v1/cnpj/".$cnpj);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Comente esta linha quando o seu site estiver rodando em https
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $retorno = curl_exec($ch);
 curl_close($ch);
